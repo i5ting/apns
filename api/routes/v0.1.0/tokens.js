755eecb87c0c1290b71c47f6f13a6e34e5ab3bed
alfred sang <i5ting@126.com>
2014-10-12 17:16:25 +0800

    refact

--- api/routes/v0.1.0/tokens.js
+++ api/routes/v0.1.0/tokens.js
@@ -30,13 +30,6 @@ router.get('/create', function(req, res) {
 router.get('/', function(req, res) {
 	var db = req.db;
 	var model = req.model
-	
-	var fluffy = new model.TokenModel({ 
-		app: 'miyou' ,
-		version: 'v0.1.0',
-		os: 'ios',
-		token: 'B1504D99F0C0DB8048709EC58BACD4A1054CB331C57627A042E72CE1DFC6873F'
-	});
 
 	// Tank.find({ size: 'small' }).where('createdDate').gt(oneYearAgo).exec(callback);
 	model.TokenModel.find().exec(function(err,docs){
